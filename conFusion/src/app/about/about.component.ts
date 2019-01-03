bf586430df90ef76de8b268d157c395ae7cc23eb
Ryškalas Porito <ryskalas.porito@gmail.com>
2019-01-03 13:44:58 +0200

    assignment 4

--- conFusion/src/app/about/about.component.ts
+++ conFusion/src/app/about/about.component.ts
@@ -19,6 +19,7 @@ import { flyInOut, expand } from '../animations/app.animation';
 })
 export class AboutComponent implements OnInit {
   leaders: Leader[];
+  errMess: string;
 
   constructor(
     private leaderService: LeaderService,
@@ -28,6 +29,6 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
     this.leaderService
       .getLeaders()
-      .subscribe(leaders => (this.leaders = leaders));
+      .subscribe(leaders => this.leaders = leaders, errmess => this.errMess = <any>errmess);
   }
 }
--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
-import { flyInOut } from '../animations/app.animation';
+import { flyInOut, expand } from '../animations/app.animation';
+import { FeedbackService } from '../services/feedback.service';
 
 @Component({
   selector: 'app-contact',
@@ -13,12 +14,18 @@ import { flyInOut } from '../animations/app.animation';
     'style': 'display: block;'
   },
   animations: [
-    flyInOut()
+    flyInOut(),
+    expand()
   ]
 })
 export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
+  feedbackCopy: Feedback;
+  errMess: string;
+
+  isFormVisible = true;
+
   contactType = ContactType;
 
   formErrors = {
@@ -51,11 +58,11 @@ export class ContactComponent implements OnInit {
 
   @ViewChild('fform') feedbackFormDirective: any;
 
-  constructor(private fb: FormBuilder, @Inject('BaseURL') private BaseURL) {
+  constructor(private fb: FormBuilder, private feedbackService: FeedbackService, @Inject('BaseURL') private BaseURL) {
     this.createForm();
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   createForm(): void {
     this.feedbackForm = this.fb.group({
@@ -102,7 +109,19 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.isFormVisible = false;
     this.feedback = this.feedbackForm.value;
+
+    this.feedbackService.submitFeedback(this.feedback)
+      .subscribe(feedback => {
+        this.feedbackCopy = feedback;
+        setTimeout(() => {
+          this.feedbackCopy = null;
+          this.isFormVisible = true;
+        }, 5000);
+      }, errmess => this.errMess = <any>errmess);
+
+    this.feedbackFormDirective.resetForm();
     this.feedbackForm.reset({
       firstname: '',
       lastname: '',
@@ -112,6 +131,5 @@ export class ContactComponent implements OnInit {
       contacttype: 'None',
       message: ''
     });
-    this.feedbackFormDirective.resetForm();
   }
 }
